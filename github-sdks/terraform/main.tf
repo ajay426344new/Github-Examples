1edb2a7ad41afc456890fb550c46e0ba8c180b4c
Ajay kumar A <ajay426344@gmail.com>
2024-02-22 23:40:47 +0530

    Added Terraform provider

---
+++ github-sdks/terraform/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the GitHub Provider
+provider "github" {}
+
+resource "github_branch" "test" {
+    repository = "Github-Examples"
+    branch = "sdks"
+  
+}
